0075e77cbe5f9e4b8917aa9da35564718321d4df
ivan <gired.bolivia@gmail.com>
2025-08-28 18:13:52 -0400

    work

--- ecommerce_app/database/seeders/DatabaseSeeder.php
+++ ecommerce_app/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Spatie\Permission\Models\Role;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,10 +15,17 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
+        
+        /*User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]);*/
+        Role::query()->delete();
+
+        // 1. Crear los roles principales
+        Role::create(['name' => 'super-admin']);
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'vendedor']);
+        Role::create(['name' => 'cliente']);
     }
 }
